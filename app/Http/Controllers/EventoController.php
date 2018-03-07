1317889a0319e8dee162331707d804467dae79de
Porfirio Ángel Díaz Sánchez <porfirioads@gmail.com>
2018-03-07 08:13:20 -0600

    Cambia formato en que devuelve las fechas de los eventos

--- app/Http/Controllers/EventoController.php
+++ app/Http/Controllers/EventoController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\CategoriaEvento;
 use App\Evento;
+use DateTime;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -12,7 +13,7 @@ class EventoController extends Controller
     public function obtenerEventos()
     {
         // TODO Determine which past events will be filtered
-        
+
         $eventos = DB::table('evento')
             ->orderBy('fecha_inicio')
             ->get();
@@ -21,8 +22,12 @@ class EventoController extends Controller
             $categoria = CategoriaEvento::find($evento->categoria_evento_id);
             $evento->categoria = $categoria->categoria;
             unset($evento->categoria_evento_id);
+            $fecha_inicio = new DateTime($evento->fecha_inicio);
+            $fecha_fin = new DateTime($evento->fecha_fin);
+            $evento->fecha_inicio = $fecha_inicio->format('d/m/Y h:i a');
+            $evento->fecha_fin = $fecha_fin->format('d/m/Y h:i a');
         }
-
+        
         return ResponseUtils::jsonResponse(200, $eventos);
     }
 }
