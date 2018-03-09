d8f3223805e0367864b41e6b53d9464931472446
Porfirio Ángel Díaz Sánchez <porfirioads@gmail.com>
2018-03-08 20:38:20 -0600

    Obtiene promociones de todos los negocios y de negocios específicos

--- app/Http/Controllers/PromocionNegocioController.php
+++ app/Http/Controllers/PromocionNegocioController.php
@@ -2,14 +2,39 @@
 
 namespace App\Http\Controllers;
 
+use App\Negocio;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Http\Validators\NegocioExistente;
 use Validator;
+use DateTime;
 
 class PromocionNegocioController extends Controller
 {
     public function obtenerPromociones(Request $request) {
+        $negocios = DB::table('negocio as n')
+            ->join('suscripcion as s', 's.id', '=', 'n.suscripcion_id')
+            ->select(['n.id', 'n.nombre', 'n.latitud', 'n.longitud', 'n.url_logo'])
+            ->where('s.fecha_fin', '>=', new DateTime())
+            ->get();
+
+        $negociosPromociones = [];
+
+        foreach ($negocios as $negocio) {
+            $promociones = DB::table('promocion_negocio')
+                ->where('negocio_id', '=', $negocio->id)
+                ->get();
+
+            if(count($promociones) > 0) {
+                $negocio->promociones = $promociones;
+                array_push($negociosPromociones, $negocio);
+            }
+        }
+
+        return ResponseUtils::jsonResponse(200, $negociosPromociones);
+    }
+
+    public function obtenerPromocionesNegocio(Request $request) {
         $validator = Validator::make($request->all(), [
             'negocio_id' => ['required', 'numeric', new NegocioExistente()],
         ]);
