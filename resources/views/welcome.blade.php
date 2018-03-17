1c3e5f7f205a79290a1424a93e97704c4449a3fa
Porfirio Ángel Díaz Sánchez <porfirioads@gmail.com>
2018-03-16 21:25:20 -0600

    Restablece etiqueta modificada

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel PHP
+                    Laravel
                 </div>
 
                 <div class="links">
